93dacdbdf55826ef60100258012fbebfc3094e74
Matt McCarthy <mattmccarthy@Matts-Air.home>
2018-11-21 23:09:47 -0500

    Validation logic for habits and dailies

--- src/htask.rs
+++ src/htask.rs
@@ -60,28 +60,80 @@ enum HTaskPriority {
 
 #[derive(Debug, PartialEq)]
 enum HDailyFrequency {
-    _DAILY,
+    DAILY,
     _WEEKLY
 }
 
 impl HTask {
     pub fn get_json<'a>(&self) -> &'a str {
-        r#"{
-
-        }"#
+        r#""#
     }
 
     pub fn is_valid(&self) -> bool {
-        match self.htype {
-            HTaskType::HABIT => false,
-            HTaskType::DAILY => false,
-            HTaskType::TODO => false,
+
+        if &self.htype != &HTaskType::REWARD {
+            match &self.priority {
+                None => return false,
+                _ => ()
+            }
+        }
+
+        match &self.htype {
+            HTaskType::HABIT => {
+
+                let up = &self.up;
+                match up {
+                    None => return false,
+                    _ => ()
+                }
+                let down = &self.down;
+                match down {
+                    None => return false,
+                    _ => ()
+                }
+
+
+                return true;
+            },
+            HTaskType::DAILY => {
+                let start_date = &self.start_date;
+                let every_x = &self.every_x;
+                let streak = &self.streak;
+                let repeat = &self.repeat;
+                let frequency = &self.frequency;
+
+                match start_date {
+                    None => return false,
+                    _ => ()
+                }
+                match every_x {
+                    None => return false,
+                    _ => ()
+                }
+                match streak {
+                    None => return false,
+                    _ => ()
+                }
+                match repeat {
+                    None => return false,
+                    _ => ()
+                }
+                match frequency {
+                    None => return false,
+                    _ => ()
+                }
+
+                return true;
+            },
+            HTaskType::TODO => {
+                return false;
+            },
             HTaskType::REWARD => false
         }
     }
 }
 
-fn make_habit() -> HTask {
+fn make_habit(up: bool, down: bool, prior: HTaskPriority) -> HTask {
     return HTask {
         htype: HTaskType::HABIT,
         text: String::new(),
@@ -89,20 +141,47 @@ fn make_habit() -> HTask {
         alias: None,
         notes: None,
         date: None,
-        priority: None,
+        priority: Some(prior),
         reminders: None,
         frequency: None,
         repeat: None,
         every_x: None,
         streak: None,
         start_date: None,
+        up: Some(up),
+        down: Some(down),
+        value: None
+    }
+}
+
+fn make_daily(sd: String,
+              ex: i32,
+              streak: i32,
+              rp: HDailyRepeat,
+              freq: HDailyFrequency,
+              prior: HTaskPriority
+) -> HTask {
+    return HTask {
+        htype: HTaskType::DAILY,
+        text: String::new(),
+        tags: None,
+        alias: None,
+        notes: None,
+        date: None,
+        priority: Some(prior),
+        reminders: None,
+        frequency: Some(freq),
+        repeat: Some(rp),
+        every_x: Some(ex),
+        streak: Some(streak),
+        start_date: Some(sd),
         up: None,
         down: None,
         value: None
     }
 }
 
-fn make_daily() -> HTask {
+fn _make_empty_task() -> HTask {
     return HTask {
         htype: HTaskType::DAILY,
         text: String::new(),
@@ -187,7 +266,7 @@ mod tests {
     #[test]
     fn test_get_priority_value() {
         assert_eq!("0.1", get_priority_value(HTaskPriority::TRIVIAL));
-        assert_eq!("1.0", get_priority_value(HTaskPriority::EASY));
+        assert_eq!("1", get_priority_value(HTaskPriority::EASY));
         assert_eq!("1.5", get_priority_value(HTaskPriority::MEDIUM));
         assert_eq!("2.0", get_priority_value(HTaskPriority::HARD));
 
@@ -196,40 +275,48 @@ mod tests {
 
     #[test]
     fn test_htask_is_valid() {
-        let habit = make_habit();
-        let daily = make_daily();
-        let todo = make_todo();
-        let reward = make_reward();
+        let mt_task = _make_empty_task();
+        let habit = make_habit(true, false, HTaskPriority::HARD);
+        let repeat = HDailyRepeat {
+            su: false,
+            m: true,
+            t: false,
+            w: true,
+            th: false,
+            f: false,
+            s: false
+        };
+        let daily = make_daily("asdf".to_string(),
+                               0,
+                               0,
+                               repeat,
+                               HDailyFrequency::DAILY,
+                                HTaskPriority::EASY
+        );
+        let _todo = make_todo();
+        let _reward = make_reward();
 
         assert_eq!(HTaskType::HABIT, habit.htype);
         assert_eq!(String::new(), habit.text);
-        assert_eq!(None, habit.tags);
-        assert_eq!(None, habit.alias);
-        assert_eq!(None, habit.notes);
-        assert_eq!(None, habit.date);
-        assert_eq!(None, habit.priority);
-        assert_eq!(None, habit.reminders);
-        assert_eq!(None, habit.frequency);
-        assert_eq!(None, habit.repeat);
-        assert_eq!(None, habit.every_x);
-        assert_eq!(None, habit.streak);
-        assert_eq!(None, habit.start_date);
-        assert_eq!(None, habit.up);
-        assert_eq!(None, habit.down);
-        assert_eq!(None, habit.value);
 
+        assert_eq!(false, mt_task.is_valid());
         assert_eq!(true, habit.is_valid());
         assert_eq!(true, daily.is_valid());
-        assert_eq!(true, todo.is_valid());
-        assert_eq!(true, reward.is_valid());
+
+
+
+
+        //assert_eq!(true, daily.is_valid());
+        //assert_eq!(true, todo.is_valid());
+        //assert_eq!(true, reward.is_valid());
 
     }
 
     #[test]
     fn test_get_json() {
-        let habit = make_habit();
+        let empty_task = _make_empty_task();
 
-        assert_eq!(String::new(), habit.get_json())
+        assert_eq!(String::new(), empty_task.get_json())
     }
 
 }
